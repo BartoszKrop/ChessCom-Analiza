3a8595bf4c463c601e64d6937689815615c9fdad
BartoszKrop <krop.bartosz@gmail.com>
2026-04-04 16:35:52 +0200

    app.py aktualisieren

--- app.py
+++ app.py
@@ -992,7 +992,6 @@ else:
                             f"</div>"
                             f"<div style='text-align: center; width: 33%; border-left: 1px solid {grid_color}; border-right: 1px solid {grid_color}; padding: 0 10px;'>"
                             f"<div style='color: {font_color}; font-size: 0.8rem; font-weight: bold; margin-bottom: 8px;'>REMISY: {h2_d}</div>"
-                            f"<div style='color: {font_color}; font-size: 0.8rem; font-weight: bold;'>RAZEM:<br><span style='font-size: 1.1rem; color: {font_color}; display: inline-block; margin-top: 4px;'>{len(h2)}</span></div>"
                             f"</div>"
                             f"<div style='text-align: left; width: 33%;'>"
                             f"<div style='color: {cp2}; font-size: 1.1rem; margin-bottom: 5px; font-weight: bold; overflow: hidden; text-overflow: ellipsis;'>{u2}</div>"
